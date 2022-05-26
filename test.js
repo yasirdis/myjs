dc2f0da982396e7241a41e3a88ccf92dca646904
yasirdis <yasir.dis@gmail.com>
2022-05-26 17:14:27 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-   window.onload=( function() {
-var widget = document.getElementById("add-to-cart-wrapper");
-widget.innerHTML = '<button id="mahbutton">Clicky</button>';
+window.onload=( function() {//from w  ww  .  ja  va 2  s  .com
+var widget = document.getElementsByClassName("price price--withTax");
+widget[0].innerHTML = '<button id="mahbutton">Clicky</button>';
 var w_btn = document.getElementById("mahbutton");
 w_btn.addEventListener("click",
     function() {
