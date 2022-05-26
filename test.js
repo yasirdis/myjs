62289449cc9f72bd6d2f5c8b2d0a781a37092b99
yasirdis <yasir.dis@gmail.com>
2022-05-26 15:30:04 +0530

    button added

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
-window.onload = function() {
-  alert("hhhhhhhhhhhhhhhhhhh");
-}
+window.onload=( function() {
+  window.onload=( function() {
+var widget = document.getElementById("add-to-cart-wrapper");
+widget.innerHTML = '<button id="mahbutton">Clicky</button>';
+var w_btn = document.getElementById("mahbutton");
+w_btn.addEventListener("click",
+    function() {
+        alert("sdf");
+    }
+);
+    });
+    });
