a25fe905d9d1a465ea724ac0efe48040210826a3
yasirdis <yasir.dis@gmail.com>
2022-05-27 12:45:27 +0530

    model added

--- test.js
+++ test.js
@@ -1,10 +1,33 @@
-window.onload=( function() {//from w  ww  .  ja  va 2  s  .com
-var widget = document.getElementsByClassName("price price--withTax");
-widget[0].innerHTML = '<button id="mahbutton">Clicky</button>';
-var w_btn = document.getElementById("mahbutton");
-w_btn.addEventListener("click",
-    function() {
-        alert("sdf");
-    }
-);
+window.onload=( function() {
+var widget = document.getElementsByClassName("productView-options");
+var model = '<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">'+
+  '<div class="modal-dialog">'+
+    '<div class="modal-content">'+
+     ' <div class="modal-header">'+
+      '  <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>'+
+       ' <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>'+
+      '</div>'+
+      '<div class="modal-body">'+
+       'Store 1 '+
+       '<br>'+
+       'Store 2 '+
+       '<br>'+
+       'Store 3 '+
+       '<br>'+
+       'Store 4 '+
+       '<br>'+
+       'Store 5 '+
+       '<br>'+
+       'Store 6 '+
+      '</div>'+
+      '<div class="modal-footer">'+
+       ' <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>'+
+     ' </div>'+
+    '</div>'+
+  '</div>'+
+'</div>';
+widget[0].innerHTML = widget[0].innerHTML + 
+			'<br><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">BOPIS</button>'+
+			model;
+
     });
